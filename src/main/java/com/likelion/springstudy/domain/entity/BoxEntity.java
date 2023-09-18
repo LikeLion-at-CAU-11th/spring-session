1cf5984083b4a5fbc79b5341ecddf5c3f4cecf18
unanchoi <unanc2@gmail.com>
2023-09-18 20:54:02 +0900

    [WEEK3] add validate letteLimit logic

--- src/main/java/com/likelion/springstudy/domain/entity/BoxEntity.java
+++ src/main/java/com/likelion/springstudy/domain/entity/BoxEntity.java
@@ -5,11 +5,14 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
 
 @Entity
 @Table(name = "letter_box")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BoxEntity {
+
+    private static final int DEFAULT_LETTER_LIMIT = 20;
 
     @Id @GeneratedValue
     private Long id;
@@ -17,7 +20,7 @@ public class BoxEntity {
     @Column(nullable = false)
     private String name;
 
-    private int letterLimit = 20;
+    private int letterLimit = DEFAULT_LETTER_LIMIT;
 
     private String code;
 
@@ -29,5 +32,25 @@ public class BoxEntity {
     public BoxEntity(String name, int letterLimit) {
         this.name = name;
         this.letterLimit = letterLimit;
+    }
+
+    @Builder
+    public BoxEntity(String name, int letterLimit, String code, MemberEntity member) {
+        validate(letterLimit);
+        this.name = name;
+        this.letterLimit = letterLimit;
+        this.code = code;
+        this.member = member;
+    }
+
+    private void validate(int letterLimit) {
+        validateLetterLimit(letterLimit);
+    }
+
+
+    private void validateLetterLimit(int letterLimit) {
+        if (letterLimit > DEFAULT_LETTER_LIMIT || letterLimit < 0) {
+           throw new IllegalArgumentException("Invalid Letter Limit");
+        }
     }
 }
