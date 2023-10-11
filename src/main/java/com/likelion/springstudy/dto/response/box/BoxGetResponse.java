0fa324c16621326f4fb0e40802d76f6f50a96633
Unan <unanc2@gmail.com>
2023-10-11 06:16:15 +0000

    Update BoxGetResponse.java

--- src/main/java/com/likelion/springstudy/dto/response/box/BoxGetResponse.java
+++ src/main/java/com/likelion/springstudy/dto/response/box/BoxGetResponse.java
@@ -1,12 +1,16 @@
 package com.likelion.springstudy.dto.response.box;
 
 import com.likelion.springstudy.domain.entity.BoxEntity;
+import com.likelion.springstudy.dto.response.letter.LetterGetResponse;
+
+import java.util.List;
 
 public record BoxGetResponse(
-        String name
+        String name,
+        List<LetterGetResponse> letters
 ) {
 
-    public static BoxGetResponse of(BoxEntity box) {
-        return new BoxGetResponse(box.getName());
+    public static BoxGetResponse of(BoxEntity box, List<LetterGetResponse> letters) {
+        return new BoxGetResponse(box.getName(), letters);
     }
 }
